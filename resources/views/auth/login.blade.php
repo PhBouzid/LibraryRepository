f1bda41ca0be49edc45e4f83f8826d0189226db4
sindbad342@gmail.com <sindbad342@gmail.com>
2021-01-12 18:27:45 +0600

    merge conflict

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,39 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Электронная библиотека</title>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet">
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.bundle.min.js"></script>
-    <script>
-        function login(){
-            if($("#inputEmail").val().length < 1){
-                alert("Поле email не может быть пустым")
-            }
-            if($("#inputPassword").val().length < 1){
-                alert("Поле пароль не может быть пустым")
-            }
-            if($("#inputPassword").val().length > 0 && $("#inputEmail").val().length > 0){
-                window.location.href = "library.html"
-            }
-        }
-    </script>
-</head>
-<body>
+@extends('master')
+
+@section('title','Авторизация')
+
+@section('content')
     <div class="container">
         <div class="card card-container">
             <h4 class="text-center mb-3">Авторизация</h4>
                     <form class="form-signin" method="POST" action="{{ route('login') }}">
                         <span id="reauth-email" class="reauth-email"></span>
-                        <label for="email" :value="__('Email')" />
+                        <label for="email">Email</label>
                         <input id="inputEmail" class="form-control" placeholder="Email"  type="email" name="email" :value="old('email')" required autofocus />
                         <!-- Password -->
-                        <label for="password" :value="__('Password')"/>
+                        <label for="password">Пароль</label>
                         <input id="inputPassword" class="form-control" placeholder="Пароль"
                                             type="password"
                                             name="password"
@@ -45,14 +23,16 @@
                             </label>
                         </div>
                                 <a class="forgot-password" href="{{route('password.request')}}">
-                                    {{__('Forgot your password?')}}
+                                    Забыли пароль?
                                 </a>
-                        <button class="btn btn-lg btn-primary btn-block btn-signin" type="button">
-                            {{__('Login')}}
+                        <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">
+                            Вход
                         </button>
                 </form>
-            </h4>
         </div>
     </div>
-</body>
-</html>
+    @endsection
+
+    @section('footscript')
+        @parent
+    @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,39 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@extends('master')
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
+@section('title','Авторизация')
+@section('content')
+<div class="container">
+    <div class="card card-container">
+        <h4 class="text-center mb-3">Регистрация</h4>
+       <form method="POST" action="{{ route('register') }}">
+                <label for="name">Имя</label>
+                <input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus />
+                <label for="surname">Имя</label>
+                <input id="surname" class="form-control" type="text" name="name" :value="old('name')" required autofocus />d
+                <label for="email">Email</label>
+                <input id="email" class="form-control" type="email" name="email" :value="old('email')" required />
+                <label for="password">Пароль</label>
+                <input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
                                 required autocomplete="new-password" />
-            </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <label for="password_confirmation">Подтверждение</label>
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
+                <input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required />
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">
+                    Регистрация
+                </button>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+</div>
+</div>
+@endsection
+
+@section('footscript')
+@parent
+@endsection
